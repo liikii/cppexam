97472954c335b74f02e5b8ce2f19b0f16e943ab9
liikii <421093387@qq.com>
2019-11-25 14:45:53 -0500

    up

---
+++ cpp309.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+ 
+int main() {
+	int i = 0;   
+	int j = 0;   
+	cout << ++i << endl; // Pre-increment   
+	cout << j++ << endl; // Post-increment
+	cout << --i << endl; // Pre-decrement   
+	cout << j-- << endl; // Post decrement 
+}
+
